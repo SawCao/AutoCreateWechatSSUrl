1ed433fc3779a1c60edf7d2324d19472299f98a7
sawCao <q649691009@hotmail.com>
2019-10-24 14:14:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ import pyperclip
 from requests_toolbelt import MultipartEncoder
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+from watchdog.events import *
+from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 
-
+last_pic = ""
 class FileEventHandler(FileSystemEventHandler):
     def __init__(self):
         FileSystemEventHandler.__init__(self)
+        
 
     def on_created(self, event):
+
         if event.is_directory:
             print("directory created:{0}".format(event.src_path))
         else:
-            print("file created:{0}".format(event.src_path))
-            if "微信截图" in event.src_path:
+            global last_pic
+            if "微信截图" in event.src_path and last_pic != event.src_path:
+                 print("file created:{0}".format(event.src_path))
                 headers = {"Connection": "keep-alive",
                            "Content-Length": "25713",
                            "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -30,10 +35,8 @@ class FileEventHandler(FileSystemEventHandler):
                            "Accept-Encoding": "gzip, deflate, br",
                            "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"}
 
-                time.sleep(2)
-                print(event.src_path)
+                time.sleep(1)
                 filePath = event.src_path
-                print(filePath)
                 file_payload = {'smfile':("xxx.png",open(event.src_path,'rb'),"image/png")}
                 m = MultipartEncoder(file_payload)
                 headers['Content-Type'] = m.content_type
@@ -42,6 +45,7 @@ class FileEventHandler(FileSystemEventHandler):
                                   headers=headers, verify=False)
                 print(r.json())
                 pyperclip.copy(r.json()['data']['url'])
+                last_pic = event.src_path
 
 
 if __name__ == "__main__":
@@ -52,12 +56,12 @@ if __name__ == "__main__":
     print('当前使用的微信截图路径为： ' + path)
     event_handler = FileEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path, recursive=False)
     observer.start()
     print("···············开始检测···············")
     try:
         while True:
-            time.sleep(1)
+            time.sleep(5)
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
